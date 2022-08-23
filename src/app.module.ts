53c7873cfeaba5ae99b1ac0b18451337acaa3ec5
hojun <ybw0619@naver.com>
2022-08-24 03:34:00 +0900

    feat: initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'codejuice.cj37miwj1alp.ap-northeast-2.rds.amazonaws.com',
+      port: 3306,
+      username: 'codejuice',
+      password: 'codejuice1234',
+      database: 'codejuice',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
